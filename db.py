b8e5e3de1c308fe7e71d102bea16eb760eb609f1
Valdrin Jonuzi <valdrinj00@gmail.com>
2024-03-17 00:14:26 -0700

    Add database schema and access functions, display content on index page

---
+++ db.py
@@ -0,0 +1,33 @@
+import click, sqlite3
+
+from flask import current_app, g
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE']
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def init_db():
+    db = get_db()
+
+    with open('./schema.sql') as f:
+        db.executescript(f.read())
+
+@click.command('init-db')
+def init_db_command():
+    init_db()
+    click.echo('Initialized the database.')
+
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db_command)
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
--- fleta.py
+++ fleta.py
@@ -1,7 +1,19 @@
+import db
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_mapping(
+    SECRET_KEY='not-so-fast',
+    DATABASE='./fleta.sqlite'
+)
+db.init_app(app)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    database = db.get_db()
+    posts = database.execute(
+        "SELECT * FROM post"
+    )
+    return render_template('index.html', posts=posts)
