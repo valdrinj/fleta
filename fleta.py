ffa7c628f80ba9f479124ca0c5e7fc8f889378a6
Valdrin Jonuzi <valdrinj00@gmail.com>
2024-03-18 20:27:54 -0700

    Add post view, create base template

--- fleta.py
+++ fleta.py
@@ -1,6 +1,6 @@
 import db
 
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 
 app = Flask(__name__)
 app.config.from_mapping(
@@ -14,6 +14,20 @@ db.init_app(app)
 def index():
     database = db.get_db()
     posts = database.execute(
-        "SELECT * FROM post"
+        '''SELECT * 
+        FROM post'''
     )
     return render_template('index.html', posts=posts)
+
+@app.route('/post/<int:post_id>')
+def get_post(post_id):
+    database = db.get_db()
+    post = database.execute(
+        '''SELECT * 
+        FROM post 
+        WHERE id = ?''',
+        (post_id,)
+    ).fetchone()
+    if post is not None:
+        return render_template('post.html', post=post)
+    return redirect(url_for('index'))
